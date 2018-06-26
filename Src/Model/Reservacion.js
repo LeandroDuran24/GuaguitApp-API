e2eeebf24d30a8f7ae20bb6deef910f3f25942ca
LEANDRO\leand <leandro_duran@ucne.edu.do>
2018-06-26 09:14:56 -0400

    creacion de modelo de reservacion

---
+++ Src/Model/Reservacion.js
@@ -0,0 +1,124 @@
+const conexion= require('./../Connection/conexion');
+
+let ReservacionModel = {}
+
+ReservacionModel.getReservaciones = (callback)=>{
+
+    if(conexion)
+    {
+
+        conexion.query('select * from Reservaciones',(err,result)=>{
+
+               if(err)
+               {
+                   callback(err,null);
+               } 
+               else
+               {
+                    callback(null,result);
+               }
+        });
+    }
+
+
+}
+
+ReservacionModel.getReservacion =(idReservacion, callback)=>{
+
+    if(conexion)
+    {
+        conexion.query('select *from Reservaciones where idReservacion= ?', idReservaciones,(err,result)=>{
+
+            if(err)
+            {
+                callback(err,null);
+            }
+            else
+            {
+                callback(null,result);
+            }
+        })
+    }
+
+
+}
+
+
+ReservacionModel.insertReservacion =(reservaciones, callback)=>{
+
+    if(conexion)
+    {
+
+        conexion.query('insert into Reservaciones ?',eservaciones,(err,result)=>{
+
+            if(err)
+            {
+                callback(err,{'InsertId': 0});
+            }
+            else
+            {
+                callback(null,{'InsertId':result.InsertId});
+            }
+        })
+    }
+}
+
+
+ReservacionModel.updateReservaciones = (reservaciones,callback)=>{
+
+    if(conexion)
+    {
+
+        const sql = `
+            update Usuarios set
+            idUsuario =${conexion.escape(reservaciones.idUsario)},
+            idTurno=${conexion.escape(reservaciones.idTurno)},
+            estado =${conexion.escape(reservaciones.estado)},
+            where idReservaciones =${reservaciones.idReservacion}`;
+
+        conexion.query(sql,(err,result)=>{
+
+            if(err)
+            {
+                throw err;
+            }
+            else
+            {
+                callback(null,{msg:'Actualizado'});
+            }
+        });
+    }
+}
+
+ReservacionModel.deleteReservaciones = (reservaciones,callback)=>{
+    if(conexion)
+    {
+        let sql =`select * from Reservaciones where idReservacion =${conexion.escape(reservaciones.idReservacion)}`;
+
+        conexion.query(sql,(err,result)=>{
+
+        if(result)
+        {
+            let sql =` delete * from Reservaciones where idReservacion =${conexion.escape(reservaciones.idReservacion)}`;
+
+            conexion,query(sql,(err,result)=>{
+
+                if(err)
+                {
+                    throw err;
+                }
+                else
+                {
+                    callback(null,{msg:"Eliminado"});
+                }
+            })
+        }
+        else
+        {
+            callback(null,{msg:"No Existe"});
+        }
+
+        });
+    }
+
+}
